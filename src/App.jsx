fc0843051fa2a979fb66e8918a76d210a06640ac
Chirag Vasava <149458923+ChiragVasava@users.noreply.github.com>
2025-08-15 00:20:05 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Navbar from './component/Navbar'
+import Manager from './component/Manager'
+import Footer from './component/Footer'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+        <Navbar />
+        <Manager />
+        <Footer />
+    </>
+  )
+}
+export default App
